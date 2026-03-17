1ca4a8f779f30e72d7700697e72eec55337cf956
SebasCeba <ceballossebastian1239@gmail.com>
2026-03-16 18:50:22 -0700

    Updated the player sprite
    The player can look both left and right, jump up and land perfectly fine without using any events.  Some of the animation/sprites provided weren't needed as you can flip the sprite itself and it would look perfectly fine. Maybe I'll add sound for when the player is walking as it's bare. Also hidden walls lol.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb2D;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Animator anim;
+
+    private float horizontalInput;
+    [SerializeField]
+    private float speed = 8f;
+    [SerializeField]
+    private float jumpingPower = 16f; 
+    private bool isFacingRight = true;
+
+    private void Update()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        anim.SetFloat("Speed", Mathf.Abs(horizontalInput));
+
+        // This checks if the player is touching the ground and checks if it could.
+        if(Input.GetKeyDown(KeyCode.W) && IsGrounded())
+        {
+            rb2D.velocity = new Vector2(rb2D.velocity.x, jumpingPower);
+            anim.SetBool("Jumping", true);
+        }
+
+        // Depending on how long or short the player holds the button, they longer/high they jump. 
+        if (Input.GetKeyUp(KeyCode.W) && rb2D.velocity.y > 0f)
+        {
+            rb2D.velocity = new Vector2(rb2D.velocity.x, rb2D.velocity.y * 0.5f);
+            anim.SetBool("Jumping", true);
+        }
+
+        Flip();
+
+        anim.SetBool("Jumping", !IsGrounded());
+    }
+
+    private void FixedUpdate()
+    {
+        rb2D.velocity = new Vector2(horizontalInput * speed, rb2D.velocity.y);
+    }
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    private void Flip()
+    {
+        if(isFacingRight && horizontalInput < 0f || !isFacingRight && horizontalInput > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale; 
+        }
+    }
+}
